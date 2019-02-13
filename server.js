f045224d2ef6a6f027b9d7461e22875c16fc451f
HMW <howardmwiener@gmail.com>
2019-02-13 11:41:59 -0500

    changed header

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
-  console.log(log);
-  fs.appendFile('serverlog.txt', log + '\n', (err) => {
-    if (err) {
-      console.log('Can\'t append to server log file');
-    }
-  });
-  next();
-});
+// app.use((req, res, next) => {
+//   var now = new Date().toString();
+//   var log = `${now}: ${req.method} ${req.url}`;
+//   console.log(log);
+//   fs.appendFile('serverlog.txt', log + '\n', (err) => {
+//     if (err) {
+//       console.log('Can\'t append to server log file');
+//     }
+//   });
+//   next();
+// });
 
 // app.use((req, res, next) => {
 //   res.render('maint.hbs', {
